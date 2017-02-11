48bd20e2b14a2b8a6e2e9c0967df4ebec3525f5f
Leekyeongwon <windgard@naver.com>
2017-02-12 05:58:34 +0900

    Update Notice Directory

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -44,21 +44,11 @@ constructor($http) {
 
   $onInit() {
     this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      this.socket.syncUpdates('thing', this.awesomeThings);
+
     });
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  
 }
 
 angular.module('mannamApp')
