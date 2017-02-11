1544c51b926e697a07898d7c9f38ded056553527
Leekyeongwon <windgard@naver.com>
2017-02-11 19:10:35 +0900

    Update main

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -8,32 +8,38 @@ constructor($http) {
   this.$http = $http;
   this.awesomeThings = [];
 
-  this.notices = [
-  {
-    target : '넥스터즈 전체',
-    title : '1월 3주차 정기모임 안내',
-    place : '창의재단',
-    time : '2017.01.04'
-  },
-  {
-    target : 'mannam',
-    title : '이번주 미팅 공지',
-    place : '아지트',
-    time : '2017.01.04'
-  },
-  {
-    target : 'somebody',
-    title : 'ux 스터디 함께할 친구들',
-    place : '',
-    time : ''
-  },
-  {
-    target : 'somebody',
-    title : '서비스를 위한 설문조사',
-    place : '',
-    time : ''
-  }
-];
+ this.notices = [
+    {
+      type : '#4EBEDA',
+      target : '넥스터즈 전체', 
+      title : '1월 3주차 정기모임 안내',
+      writer : '넥스터즈 CEO',
+      location : '창의재단',
+      time : '2017.01.04 | 17:04'
+    },
+    {
+      type : '#4DB0A6',
+      target : 'mannam', 
+      title : '이번주 미팅 공지',
+      writer : '넥스터즈 CEO',
+      location : '아지트',
+      time : '2017.01.04'
+    },
+    {
+      target : 'somebody', 
+      title : 'ux 스터디 함께할 친구들',
+      writer : 'N10 우종희',
+      location : '',
+      time : ''
+    },
+    {
+      target : 'somebody', 
+      title : '서비스를 위한 설문조사',
+      writer : 'N10 우종희',
+      location : '',
+      time : ''
+    }
+  ];
 }
 
   $onInit() {
