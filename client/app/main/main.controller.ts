12bbdfed86296fa80e80106b4677beb75f7c8e6b
BLU <chojunyeong>
2017-02-25 03:26:32 +0900

    글쓰

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -11,7 +11,7 @@ constructor($http) {
   this.notices = [
     {
         type : '#4EBEDA',
-        category : '넥스터즈 전체', 
+        category : '넥스터즈 전체',
         title : '1월 3주차 정기모임 안내',
         writer : 'Nexters CEO',
         location : '마루180',
@@ -20,7 +20,7 @@ constructor($http) {
     },
     {
         type : '#4EBEDA',
-        category : 'MANNAM 팀', 
+        category : 'MANNAM 팀',
         title : '이번주에도 아지트에서 모입니다.',
         writer : '만남대장',
         location : '창의재단',
@@ -29,14 +29,14 @@ constructor($http) {
     },
     {
         type : '#4EBEDA',
-        category : '개발자', 
+        category : '개발자',
         title : '개발 스터디 멤버를 구하고 있습니다.',
         writer : '김넥터',
         time : '17.03.12 06:00'
     },
     {
         type : '#4EBEDA',
-        category : '자유글', 
+        category : '자유글',
         title : '집에 같이 가실분 찾아요',
         writer : '김넥터',
         time : '17.03.12 06:00'
@@ -46,11 +46,11 @@ constructor($http) {
 
   $onInit() {
     this.$http.get('/api/things').then(response => {
-
+      this.notices = response.data;
     });
   }
 
-  
+
 }
 
 angular.module('mannamApp')
