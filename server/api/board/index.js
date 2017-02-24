977db6a75f4b4f768970fb0eb19b8483cce3dceb
HodongMan <arshavin3@naver.com>
2017-02-25 04:06:00 +0900

    push insert

--- server/api/board/index.js
+++ server/api/board/index.js
@@ -7,13 +7,15 @@ import * as auth from '../../auth/auth.service';
 
 var router = new Router();
 
-router.get('/:pageNumber', controller.list);
-router.get('/detail/:boardID', controller.show);
-router.post('/', controller.create);
+router.get('/:pageNumber', auth.isAuthenticated(), controller.list);
+router.get('/detail/:boardID', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/', auth.isAuthenticated(), controller.change);
 router.delete('/', auth.isAuthenticated(), controller.destroy);
 
 router.put('/favorite/create/:boardID',auth.isAuthenticated(),favorite_controller.create);
+
+router.post("/push", auth.isAuthenticated(), controller.push);
 
 
 module.exports = router;
